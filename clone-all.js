ad34c80cf011abf2c3991018140a614311fa63ab
Nikolaos Georgiou <Nikolaos.Georgiou@gmail.com>
2016-09-04 20:13:40 +0200

    Actually cloning the repos. Using promises.

--- clone-all.js
+++ clone-all.js
@@ -1,148 +1,102 @@
 #!/usr/bin/node
 
+var fs = require('fs');
 var https = require('https');
+var Promise = require('promise');
+var exec = require('child_process').exec;
 
-function dohttps(requestOptions, fn) {
-	var req = https.request(requestOptions, function(res) {
-		var message = '';
-		if (res.statusCode === 200) {
-			res.on('data', function(chunk) {
-				message += chunk;
-			});
+function dohttps(requestOptions) {
+    return new Promise(function(fullfill, reject) {
+        var req = https.request(requestOptions, function(res) {
+            var message = '';
+            if (res.statusCode === 200) {
+                res.on('data', function(chunk) {
+                    message += chunk;
+                });
 
-			res.on('end', function() {
-				fn(message);
-			});
-		} else {
-			console.error(res.statusCode);
-			res.on('data', function(d) {
-				process.stdout.write(d);
-			});
-		}
-	});
+                res.on('end', function() {
+                    fullfill(message);
+                });
+            } else {
+                reject(res.statusCode);
+                res.on('data', function(d) {
+                    process.stdout.write(d);
+                });
+            }
+        });
 
-	req.end();
+        req.end();
 
-	req.on('error', function(e) {
-		console.error(e);
-	});
+        req.on('error', function(e) {
+            reject(e);
+        });
+    });
 }
 
-function processSourceForge(requestOptions, jsonRepositories) {
-	'use strict';
-
-	var profile = JSON.parse(jsonRepositories);
-	var projects = profile.projects;
-	var projectUrl;
-	var i;
-	var cloneAllOptions = requestOptions['clone-all'] || {};
-	var forceUsername = cloneAllOptions.forceUsername || '';
-	var localFolder = cloneAllOptions.localFolder || '';
-
-	function getSourceForgeProject(projectUrl) {
-		var request = {
-			hostname: 'sourceforge.net',
-			port: 443,
-			path: '/rest' + projectUrl,
-			method: 'GET',
-			headers: {
-				'User-Agent': 'clone-all.js'
-			}
-		};
-
-		dohttps(request, function(message) {
-			var project = JSON.parse(message);
-			var tools = project.tools;
-			var shortname = project.shortname;
-			var mountPoint = '';
-			var url = '';
-			var i;
-			for (i = tools.length - 1; i >= 0; i--) {
-				if (tools[i].name === 'git') {
-					mountPoint = tools[i].mount_point;
-					break;
-				}
-			}
-
-			if (!mountPoint) {
-				return;
-			}
-
-			// RO git://git.code.sf.net/p/imagehelper/code
-			// RW ssh://ngeor@git.code.sf.net/p/imagehelper/code
-
-			url = 'ssh://' + forceUsername + '@git.code.sf.net/p/' + shortname + '/' + mountPoint;
-			console.log('git clone ' + url + ' ' + localFolder + shortname);
-		});
-	}
-
-	for (i = projects.length - 1; i >= 0; i--) {
-		projectUrl = projects[i].url;
-		getSourceForgeProject(projectUrl);
-	}
+function cloneRepo(cloneUrl, cloneLocation) {
+    return new Promise(function(fullfill, reject) {
+        fs.stat(cloneLocation, function(err, stats) {
+            if (err) {
+                exec('git clone ' + cloneUrl + ' ' + cloneLocation, function(error, stdout, stderr) {
+                    fullfill('Finished cloning ' + cloneUrl + ': ' + error);
+                });
+            } else {
+                fullfill(cloneLocation + ' already exists');
+            }
+        });
+    });
 }
 
 function processGitHub(requestOptions, jsonRepositories) {
-	var repositories = JSON.parse(jsonRepositories);
-	var repository;
-	var url;
-	var i;
-	var cloneAllOptions = requestOptions['clone-all'] || {};
-	var forceUsername = cloneAllOptions.forceUsername || '';
-	var localFolder = cloneAllOptions.localFolder || '';
+    var repositories = JSON.parse(jsonRepositories);
+    var cloneAllOptions = requestOptions['clone-all'] || {};
+    var forceUsername = cloneAllOptions.forceUsername || '';
+    var localFolder = cloneAllOptions.localFolder || '';
+    return repositories.map(function(repository) {
+        var url = repository.ssh_url; // jscs: ignore
 
-	for (i = repositories.length - 1; i >= 0; i--) {
-		repository = repositories[i];
-		url = repository.ssh_url; // jscs: ignore
+        // var url = repository.clone_url;
 
-		// var url = repository.clone_url;
+        if (forceUsername) {
+            url = url.replace('ssh://', 'ssh://' + forceUsername + '@');
+        }
 
-		if (forceUsername) {
-			url = url.replace('ssh://', 'ssh://' + forceUsername + '@');
-		}
+        var cloneLocation = '';
+        if (localFolder) {
+            cloneLocation = localFolder + repository.name;
+        }
 
-		if (localFolder) {
-			console.log('git clone ' + url + ' ' + localFolder + repository.name);
-		} else {
-			console.log('git clone ' + url);
-		}
-	}
+        return cloneRepo(url, cloneLocation);
+    });
 }
 
 /**
  * Fetches repository information from GitHub.
  */
 function getRepositoryInfo(requestOptions) {
-	dohttps(requestOptions, function(jsonRepositories) {
-		var handler;
-		if (requestOptions.hostname === 'sourceforge.net') {
-			handler = processSourceForge;
-		} else {
-			handler = processGitHub;
-		}
-
-		handler(requestOptions, jsonRepositories);
-	});
+    return dohttps(requestOptions).then(function(jsonRepositories) {
+        return Promise.all(processGitHub(requestOptions, jsonRepositories));
+    });
 }
 
-(function() {
-	'use strict';
+fs.readFile('clone-all-config.json', 'utf8', function(err, data) {
+    var servers;
 
-	var fs = require('fs');
-	fs.readFile('clone-all-config.json', 'utf8', function(err, data) {
-		var servers;
-		var i;
+    if (err) {
+        throw err;
+    }
 
-		if (err) {
-			throw err;
-		}
-
-		servers = JSON.parse(data);
-
-		// process every server in configuration
-		for (i = servers.length - 1; i >= 0; i--) {
-			getRepositoryInfo(servers[i]);
-		}
-	});
-
-}());
+    servers = JSON.parse(data);
+    var promises = servers.map(function(server) {
+        return getRepositoryInfo(server);
+    });
+    Promise.all(promises).then(function(data) {
+        // flatten array
+        var newData = data.reduce(function(previousValue, currentValue) {
+            return previousValue.concat(currentValue);
+        }, []);
+        console.log(newData);
+    }).catch(function(err) {
+        console.error(err);
+    });
+});
