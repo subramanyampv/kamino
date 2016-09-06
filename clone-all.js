b9397d40c225755fff9b93d6f1a00adc3d2d5e4b
Nikolaos Georgiou <Nikolaos.Georgiou@gmail.com>
2016-09-06 20:16:38 +0200

    Implemented pagination support for github

--- clone-all.js
+++ clone-all.js
@@ -4,7 +4,12 @@ var fs = require('fs');
 var https = require('https');
 var Promise = require('promise');
 var exec = require('child_process').exec;
+var _ = require('lodash');
 
+/**
+ * Performs an HTTPS request and returns a promise that resolves to the
+ * body of the response.
+ */
 function dohttps(requestOptions) {
     return new Promise(function(fullfill, reject) {
         var req = https.request(requestOptions, function(res) {
@@ -37,22 +42,30 @@ function cloneRepo(cloneUrl, cloneLocation) {
     return new Promise(function(fullfill, reject) {
         fs.stat(cloneLocation, function(err, stats) {
             if (err) {
+                console.log('cloning ' + cloneLocation);
                 exec('git clone ' + cloneUrl + ' ' + cloneLocation, function(error, stdout, stderr) {
-                    fullfill('Finished cloning ' + cloneUrl + ': ' + error);
+                    console.log('finished cloning ' + cloneLocation);
+                    fullfill({
+                        cloneLocation: cloneLocation,
+                        success: !error
+                    });
                 });
             } else {
-                fullfill(cloneLocation + ' already exists');
+                console.error(cloneLocation + ' already exists');
+                fullfill({
+                    cloneLocation: cloneLocation,
+                    success: false
+                });
             }
         });
     });
 }
 
-function processGitHub(requestOptions, jsonRepositories) {
-    var repositories = JSON.parse(jsonRepositories);
+function processGitHub(requestOptions, repositories) {
     var cloneAllOptions = requestOptions['clone-all'] || {};
     var forceUsername = cloneAllOptions.forceUsername || '';
     var localFolder = cloneAllOptions.localFolder || '';
-    return repositories.map(function(repository) {
+    return Promise.all(repositories.map(function(repository) {
         var url = repository.ssh_url; // jscs: ignore
 
         // var url = repository.clone_url;
@@ -67,15 +80,29 @@ function processGitHub(requestOptions, jsonRepositories) {
         }
 
         return cloneRepo(url, cloneLocation);
-    });
+    }));
 }
 
 /**
  * Fetches repository information from GitHub.
  */
-function getRepositoryInfo(requestOptions) {
-    return dohttps(requestOptions).then(function(jsonRepositories) {
-        return Promise.all(processGitHub(requestOptions, jsonRepositories));
+function getRepositoryInfo(requestOptions, pageNumber, totalRepositories) {
+    var ro = _.cloneDeep(requestOptions);
+    pageNumber = pageNumber || 1;
+    totalRepositories = totalRepositories || [];
+    if (pageNumber > 1) {
+        ro.path = ro.path + '?page=' + pageNumber;
+    }
+
+    return dohttps(ro).then(function(jsonRepositories) {
+        return JSON.parse(jsonRepositories);
+    }).then(function(repositories) {
+        totalRepositories = totalRepositories.concat(repositories);
+        if (repositories.length <= 0 || !requestOptions['clone-all'].fetchAllPages) {
+            return totalRepositories; // last page of results
+        }
+
+        return getRepositoryInfo(requestOptions, pageNumber + 1, totalRepositories);
     });
 }
 
@@ -88,14 +115,16 @@ fs.readFile('clone-all-config.json', 'utf8', function(err, data) {
 
     servers = JSON.parse(data);
     var promises = servers.map(function(server) {
-        return getRepositoryInfo(server);
+        return getRepositoryInfo(server).then(function(repositories) {
+            return processGitHub(server, repositories);
+        });
     });
     Promise.all(promises).then(function(data) {
-        // flatten array
-        var newData = data.reduce(function(previousValue, currentValue) {
-            return previousValue.concat(currentValue);
-        }, []);
-        console.log(newData);
+        data.forEach(function(d) {
+            d.forEach(function(repositoryResult) {
+                console.log('Cloned ' + repositoryResult.cloneLocation + ', success = ' + repositoryResult.success);
+            });
+        });
     }).catch(function(err) {
         console.error(err);
     });
