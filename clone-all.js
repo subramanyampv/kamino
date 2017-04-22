9962083fc6dbf92b2bbeabb59c55c359e3e722cd
Nikolaos Georgiou <Nikolaos.Georgiou@gmail.com>
2017-04-22 20:12:31 +0200

    refactoring

--- clone-all.js
+++ clone-all.js
@@ -1,37 +1,28 @@
 #!/usr/bin/env node
 
-var Promise = require('promise');
 var repoProvider = require('./lib/repo_provider');
 var logger = require('./lib/logger');
 var gitClone = require('./lib/git_clone');
 var repositoriesToCloneInstances = require('./lib/repositories_to_clone_instances');
+var sequentialArrayPromise = require('./lib/sequential_array_promise');
 
 function summarizeErrors(cloneResult) {
     if (cloneResult.error) {
         logger.error('Error cloning ' + cloneResult.cloneLocation + ': ' + cloneResult.error);
         process.exitCode = 1;
     }
+
+    return cloneResult;
 }
 
-var results = [];
+function handleSingleRepo(cloneInstruction) {
+    return gitClone(cloneInstruction).then(summarizeErrors);
+}
+
 var mainPromise = repoProvider.getRepositories() // get repositories via REST API
     .then(repositoriesToCloneInstances) // convert results to array of clone instructions
     .then(function(cloneInstructions) {
-        return cloneInstructions.map(cloneInstruction => (()=>gitClone(cloneInstruction)));
-    })
-    .then(function(clonePromises) {
-        return clonePromises.reduce(function(accumulator, currentValue) {
-            return accumulator
-                .then(currentValue)
-                .then(function(result) {
-                    results.push(result);
-                    return result;
-                })
-                .then(summarizeErrors);
-        }, Promise.resolve());
-    })
-    .then(function() {
-        return results;
+        return sequentialArrayPromise(cloneInstructions, handleSingleRepo);
     })
     .catch(function(err) { // generic catch-all
         process.exitCode = 2;
