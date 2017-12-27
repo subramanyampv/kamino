dbe86c3dd5b1d4a55da2057f2bcc588cbda28438
admin <admin@TeamCity>
2017-12-27 13:54:49 +0000

    TeamCity change in 'Blog Helm' project: parameters of 'Deploy Stage' build configuration were updated

--- .teamcity/BlogHelm/buildTypes/BlogHelm_DeployStage.kt
+++ .teamcity/BlogHelm/buildTypes/BlogHelm_DeployStage.kt
@@ -7,4 +7,8 @@ object BlogHelm_DeployStage : BuildType({
     uuid = "2c559e38-b9b4-4dcf-a79f-2faa91c9f5af"
     id = "BlogHelm_DeployStage"
     name = "Deploy Stage"
+
+    params {
+        param("env", "to")
+    }
 })
