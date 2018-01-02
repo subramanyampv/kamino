60fb43ee1aeb3493b13e82381c1dc3784144a4cf
Nikolaos Georgiou <Nikolaos.Georgiou@gmail.com>
2018-01-02 09:05:17 +0100

    added unit test

---
+++ tests/test_args_parser.py
@@ -0,0 +1,23 @@
+'''Unit tests for argument parsing'''
+import unittest
+
+import args_parser
+
+class BasicTestCase(unittest.TestCase):
+    '''Basic unit tests for argument parsing'''
+    @unittest.expectedFailure
+    def test_no_args_should_fail(self):
+        args_parser.parse_args([])
+
+class DryRunTestCase(unittest.TestCase):
+    '''Unit tests for dry run feature'''
+    def test_dry_run_on(self):
+        args = args_parser.parse_args(['--dry-run', 'list-tags'])
+        self.assertTrue(args.dry_run)
+
+    def test_dry_run_off(self):
+        args = args_parser.parse_args(['list-tags'])
+        self.assertFalse(args.dry_run)
+
+if __name__ == '__main__':
+    unittest.main()
--- wpbot.py
+++ wpbot.py
@@ -1,10 +1,9 @@
 '''WordPress bot main module'''
-import argparse
 import re
 import requests
-import oauth
 
-DEFAULT_SITE = 'ngeor.wordpress.com'
+import oauth
+import args_parser
 
 # page size for paginated REST API requests
 PAGE_SIZE = 10
@@ -17,22 +16,7 @@ class WPBot:
 
     def _parse_args(self):
         '''Parses command line arguments'''
-        parser = argparse.ArgumentParser(description='WordPress bot')
-        parser.add_argument('--client-id', help='Client ID')
-        parser.add_argument('--client-secret', help='Client Secret')
-        parser.add_argument('-s', '--site', default=DEFAULT_SITE, help='WordPress hostname')
-        parser.add_argument('-p', '--post-id', type=int, help='Post ID to get/update')
-        parser.add_argument(
-            '--post-filter',
-            choices=['all', 'fixable', 'unfixable'],
-            default='all',
-            help='Which posts to show'
-        )
-        parser.add_argument(
-            'command',
-            choices=['list-tags', 'list-posts', 'get-post', 'fix-post'],
-            help='The command to run')
-        return parser.parse_args()
+        return args_parser.parse_args()
 
     def list_all_tags(self):
         '''Lists all tags of the blog'''
