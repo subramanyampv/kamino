7e4a54a67e519d3d941567084cee31cb04651a16
Nikolaos Georgiou <Nikolaos.Georgiou@gmail.com>
2018-01-13 13:36:27 +0100

    making unit tests work regardless of the year.

--- test/index.js
+++ test/index.js
@@ -1,7 +1,7 @@
-var path = require('path');
-var fs = require('fs');
-var helpers = require('yeoman-test');
-var assert = require('yeoman-assert');
+const path = require('path');
+const fs = require('fs');
+const helpers = require('yeoman-test');
+const assert = require('yeoman-assert');
 
 describe('app', () => {
     describe('files', () => {
@@ -39,72 +39,123 @@ describe('app', () => {
             ]);
         });
 
+        /**
+         * Checks if the given path is a directory.
+         * @param {string} path - The path to check.
+         * @returns {boolean} true if the path is a directory, false otherwise.
+         */
         function isDirectory(path) {
             var s = fs.statSync(path);
             return s.isDirectory();
         }
 
+        /**
+         * Reads recursively the contents of the given directory.
+         * @param {string} dirname - The path to read.
+         * @returns {string[]} A collection of paths.
+         */
         function readdirSyncRecursive(dirname) {
-            var result = fs.readdirSync(dirname);
-            var t = [];
-            result.map(f => path.join(dirname, f)).forEach(f => {
+            var contents = fs.readdirSync(dirname);
+            var result = [];
+            contents.map(f => path.join(dirname, f)).forEach(f => {
                 if (isDirectory(f)) {
-                    t = t.concat(readdirSyncRecursive(f));
+                    result = result.concat(readdirSyncRecursive(f));
                 } else {
-                    t.push(f);
+                    result.push(f);
                 }
             });
 
-            return t;
+            return result;
         }
 
-        function removeGuids(input) {
-            return input.replace(
-                /[0-9a-z]{8}\-[0-9a-z]{4}\-[0-9a-z]{4}\-[0-9a-z]{4}\-[0-9a-z]{12}/ig,
-                '');
+        class GuidHandler {
+            /**
+             * Checks if the file might contain GUIDs.
+             * @param {string} filename - The filename to check.
+             * @returns {boolean} A value indicating whether the file is expected to contain GUIDs.
+             */
+            shouldHandle(filename) {
+                var ext = path.extname(filename);
+                var basename = path.basename(filename);
+                return ext === '.csproj' || ext === '.sln' || basename === 'AssemblyInfo.cs';
+            }
+
+            transformActualData(input) {
+                return input.replace(
+                    /[0-9a-z]{8}\-[0-9a-z]{4}\-[0-9a-z]{4}\-[0-9a-z]{4}\-[0-9a-z]{12}/ig,
+                    '');
+            }
+
+            transformExpectedData(input) {
+                return this.transformActualData(input);
+            }
         }
 
-        /**
-         * Checks if the file might contain GUIDs.
-         * @param {string} filename - The filename to check.
-         * @returns {boolean} A value indicating whether the file is expected to contain GUIDs.
-         */
-        function expectedToContainGuids(filename) {
-            var ext = path.extname(filename);
-            var basename = path.basename(filename);
-            return ext === '.csproj' || ext === '.sln' || basename === 'AssemblyInfo.cs';
+        class DateHandler {
+            /**
+             * Checks if the file might contain the present date.
+             * @param {string} filename - The filename to check.
+             * @returns {boolean} A value indicating whether the file is expected to contain the present date.
+             */
+            shouldHandle(filename) {
+                var basename = path.basename(filename);
+                return basename === 'CHANGELOG.md';
+            }
+
+            transformActualData(actualData) {
+                return actualData.replace(
+                    new Date().toISOString().substr(0, 10),
+                    '2017-08-01'); // the date of the test data
+            }
+
+            transformExpectedData(expectedData) {
+                return expectedData;
+            }
         }
 
-        /**
-         * Checks if the file might contain the present date.
-         * @param {string} filename - The filename to check.
-         * @returns {boolean} A value indicating whether the file is expected to contain the present date.
-         */
-        function expectedToContainDate(filename) {
-            var basename = path.basename(filename);
-            return basename === 'CHANGELOG.md';
+        class YearHandler {
+            /**
+             * Checks if the file might contain the present year.
+             * @param {string} filename - The filename to check.
+             * @returns {boolean} A value indicating whether the file is expected to contain the present year.
+             */
+            shouldHandle(filename) {
+                var basename = path.basename(filename);
+                return basename === 'AssemblyInfo.cs';
+            }
+
+            transformActualData(actualData) {
+                return actualData.replace(
+                    new Date().toISOString().substr(0, 4),
+                    '2017'); // the year of the test data
+            }
+
+            transformExpectedData(expectedData) {
+                return expectedData;
+            }
         }
 
         function templateTests() {
             var expectedDataDirectory = path.join(__dirname, 'data');
             var expectedFiles = readdirSyncRecursive(expectedDataDirectory)
                 .map(f => path.relative(expectedDataDirectory, f));
+            const handlers = [
+                new GuidHandler(),
+                new DateHandler(),
+                new YearHandler()
+            ];
             expectedFiles.forEach(fixtureFile => {
                 var sourceFile = fixtureFile;
                 var destFile = fixtureFile.replace('_', '.');
                 it(`should map ${sourceFile} to ${destFile}`, () => {
                     var actualData = fs.readFileSync(destFile, 'utf8');
                     var expectedData = fs.readFileSync(path.join(expectedDataDirectory, sourceFile), 'utf8');
-
-                    if (expectedToContainGuids(sourceFile)) {
-                        actualData = removeGuids(actualData);
-                        expectedData = removeGuids(expectedData);
-                    }
-
-                    if (expectedToContainDate(sourceFile)) {
-                        actualData = actualData.replace(
-                            new Date().toISOString().substr(0, 10),
-                            '2017-08-01'); // the date of the test data
+                    for (let index = 0; index < handlers.length; index++) {
+                        const handler = handlers[index];
+                        if (handler.shouldHandle(sourceFile)) {
+                            actualData = handler.transformActualData(actualData);
+                            expectedData = handler.transformExpectedData(expectedData);
+                        }
                     }
 
                     assert.textEqual(actualData, expectedData);
