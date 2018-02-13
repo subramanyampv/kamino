b1609a9c4e1618b39a7899c72201a0ac70f6ff9b
Nikolaos Georgiou <Nikolaos.Georgiou@gmail.com>
2018-02-13 12:12:59 +0100

    Initial import.

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
 # kddbot
 A bot that automatically creates a Confluence page when a Jira ticket is created.
+
+## Requirements
+
+- Atlassian Cloud instance
+- AWS Lambda and AWS API Gateway
+- Python 3.6
+- requests python module
+
+## Preparing deployment package
+
+- Run `pip install -t .` in the working directory.
+- Create a zip file including the code and the dependencies. The zip file should not have a top-level folder.
+
+## Create AWS Lambda Function
+
+Select the following options:
+
+- Name: kddbot
+- Runtime: Python 3.6
+- Role: Create new role from template
+- Role name: kddbot
+
+## Upload deployment package
+
+- Edit the lambda function
+- Select Code Entry Type: Upload a zip file
+- Upload the zip file
+- Save function
+
+## Set environment variables
+
+Environment Variables:
+
+* ATLASSIAN_CLOUD_NAME: The name of the Atlassian Cloud site (only the name, before `.atlassian.net`)
+* USERNAME and PASSWORD: Credentials for Basic Authentication
+* SPACE_KEY: The key in which pages will be created
+* PARENT_PAGE_ID: The parent page of the newly created pages
+
+Save the function after editing the environment variables.
+
+## Create API Gateway
+
+- Create new API Gateway
+- Select New API with name kddbot
+- Add POST method on the / resource.
+- Configure POST method on the / resource to call the kddbot Lambda function.
+- Deploy the API on a new stage named prod
+
+This provides the HTTPS endpoint that needs to be configured in Jira.
+
+## Configure Jira WebHook
+
+- Configure WebHooks in Jira (System -> Advanced -> WebHooks)
+- Create a new WebHook with name kddbot
+- The URL should be the URL of the API Gateway
+- Select to listen on Issue Created events
---
+++ lambda_function.py
@@ -0,0 +1,142 @@
+import requests
+from requests.auth import HTTPBasicAuth
+import os
+
+def create_page(issue_key, summary):
+    data = {
+        'ancestors': [
+            { 'id': os.environ['PARENT_PAGE_ID'] }
+        ],
+        'body': {
+            'storage': {
+			"value": """
+            <ac:structured-macro ac:name=\"details\" ac:schema-version=\"1\" ac:macro-id=\"6e9b2ec2-f34c-4130-bf49-de9eb13d3225\">
+                <ac:parameter ac:name=\"label\" />
+                <ac:rich-text-body>
+                    <table class=\"wrapped\">
+                        <tbody>
+                            <tr>
+                                <th>Status</th>
+                                <td>
+                                    <ac:structured-macro ac:name=\"status\" ac:schema-version=\"1\" ac:macro-id=\"ab9a9bc7-e671-476b-889c-0c553ca825cb\">
+                                        <ac:parameter ac:name=\"colour\">Grey</ac:parameter>
+                                        <ac:parameter ac:name=\"title\">Not started</ac:parameter>
+                                    </ac:structured-macro>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th>Stakeholders</th>
+                                <td>
+                                    <ac:placeholder>(insert stakeholder here)</ac:placeholder>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th>Outcome</th>
+                                <td>
+                                    <ac:placeholder>What did you decide?</ac:placeholder>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th>Due date</th>
+                                <td>
+                                    <ac:placeholder>When is the due date?</ac:placeholder>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th>Owner</th>
+                                <td>
+                                    <ac:placeholder>(insert owner here)</ac:placeholder>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </ac:rich-text-body>
+            </ac:structured-macro>
+
+            <h2>Overview</h2>
+            <p>Decide on something</p>
+
+            <h2>Decision</h2>
+            <ac:structured-macro ac:name=\"tip\" ac:schema-version=\"1\" ac:macro-id=\"01fe8df3-1f03-4706-b117-cdc1b32f8b08\">
+                <ac:rich-text-body>
+                    <p>Decision</p>
+                </ac:rich-text-body>
+            </ac:structured-macro>
+
+            """,
+			"representation": "storage",
+            }
+        },
+        'space': {
+            'key': os.environ['SPACE_KEY']
+        },
+        'status': 'current',
+        'title': summary,
+        'type': 'page'
+    }
+
+    auth = HTTPBasicAuth(os.environ['USERNAME'], os.environ['PASSWORD'])
+    base_url = os.environ['ATLASSIAN_CLOUD_NAME']
+
+    # create page
+    result = requests.post(
+        f'https://{base_url}.atlassian.net/wiki/rest/api/content',
+        auth=auth,
+        headers={
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        json=data)
+    result.raise_for_status()
+    json = result.json()
+    new_page_id = json['id']
+
+    # add label 'decisions'
+    result = requests.post(
+        f'https://{base_url}.atlassian.net/wiki/rest/api/content/{new_page_id}/label',
+        auth=auth,
+        headers={
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        json=[
+            {
+                'name': 'decisions'
+            }
+        ])
+    result.raise_for_status()
+
+    # create link from jira to confluence page
+    result = requests.post(
+        f'https://{base_url}.atlassian.net/rest/api/2/issue/{issue_key}/remotelink',
+        auth=auth,
+        headers={
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        json={
+            "globalId": f"appId=cf551dd2-cf83-37ca-a19c-62fffd328e08&pageId={new_page_id}",
+            "application": {
+                "type": "com.atlassian.confluence",
+                "name": "System Confluence"
+            },
+            "relationship": "Wiki Page",
+            "object": {
+                "url": f"https://{base_url}.atlassian.net/wiki/pages/viewpage.action?pageId={new_page_id}",
+                "title": "Wiki Page"
+            }
+        }
+    )
+    result.raise_for_status()
+    return json
+
+def lambda_handler(event, context):
+    """
+    Gets updates from JIRA when an issue is created.
+    """
+    issue_key = event['issue']['key']
+    summary = event['issue']['fields']['summary']
+    if 'KDD' in summary:
+        return create_page(issue_key, summary)
+    else:
+        return { 'result': 'Not a KDD issue' }
