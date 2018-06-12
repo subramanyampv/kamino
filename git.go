e2066b9bc3a89705c85e2d3b04d61e0f13854686
Nikolaos Georgiou <Nikolaos.Georgiou@gmail.com>
2018-06-12 14:29:40 +0200

    Sorting tags numerically and not alphabetically.

--- git.go
+++ git.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"os/exec"
+	"sort"
+	"strconv"
 	"strings"
 )
 
@@ -44,6 +46,45 @@ func gitPush(gitDir string) (string, error) {
 	return gitCommand(gitDir, "push", "--follow-tags")
 }
 
+func compareSplittedSemVerTag(x, y []string) int {
+	if len(x) < len(y) {
+		return -1
+	} else if len(x) > len(y) {
+		return 1
+	}
+
+	result := 0
+
+	// normal case here, equal arrays
+	for i := 0; i < len(x); i++ {
+		xTrimmed := strings.TrimPrefix(x[i], "v")
+		yTrimmed := strings.TrimPrefix(y[i], "v")
+
+		xInt, err := strconv.Atoi(xTrimmed)
+		if err != nil {
+			// compare as strings
+			result = strings.Compare(xTrimmed, yTrimmed)
+		} else {
+			yInt, err := strconv.Atoi(yTrimmed)
+			if err != nil {
+				result = strings.Compare(xTrimmed, yTrimmed)
+			} else {
+				result = xInt - yInt
+			}
+		}
+
+		if result != 0 {
+			return result
+		}
+	}
+
+	return 0
+}
+
+func compareSemVerTag(x, y string) int {
+	return compareSplittedSemVerTag(strings.Split(x, "."), strings.Split(y, "."))
+}
+
 // LatestVersion gets the most recent version based on the git tags.
 // The 'v' prefix of the tag is removed.
 func LatestVersion(gitDir string) (string, error) {
@@ -54,6 +95,10 @@ func LatestVersion(gitDir string) (string, error) {
 	}
 
 	tagArray := strings.Split(cmdOutAsString, "\n")
+	sort.Slice(tagArray, func(i, j int) bool {
+		return compareSemVerTag(tagArray[i], tagArray[j]) < 0
+	})
+
 	for i := len(tagArray) - 1; i >= 0; i-- {
 		tag := tagArray[i]
 		if tag != "" {
