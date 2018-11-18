ebc000ff9cd6219989b1f59d2b72aebed05b70de
Nikolaos Georgiou <Nikolaos.Georgiou@gmail.com>
2018-11-18 07:39:44 +0100

    Added dry-run and dir-pattern parameters.

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # dirloop
 CLI utility that runs a command on multiple directories
+
+## Typical use case
+
+I want to run a series of git commands on multiple directories. They are all in
+the same parent directory.
+
+```
+$ dirloop git gc
+$ dirloop git fetch -p -t
+$ dirloop git checkout -- .
+$ dirloop git checkout master
+$ dirloop git reset --hard origin/master
+```
+
+## Syntax
+
+`dirloop [parameters] command [command arguments]`
+
+### Parameters
+
+- `dir` The parent directory. It contains the directories in which the command
+  will be run. Defaults to the current directory.
+- `dir-pattern` An optional wildcard pattern to select only some of the
+  sub-directories.
+- `dry-run` Don't actually run the command, just see what would happen.
+- `help` Show help about the command
--- dirloop.go
+++ dirloop.go
@@ -8,50 +8,101 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"path"
 	"path/filepath"
 )
 
-func main() {
-	var dir string
+// Arguments holds the command line arguments
+type Arguments struct {
+	Dir              string
+	DirPattern       string
+	DryRun           bool
+	CommandName      string
+	CommandArguments []string
+}
 
-	flag.StringVar(&dir, "dir", ".", "The root directory containing other directories")
+// CreateCommand creates an exec.Cmd instance based on the given arguments.
+func (a Arguments) CreateCommand() *exec.Cmd {
+	return exec.Command(a.CommandName, a.CommandArguments...)
+}
+
+// IsMatchingDir checks if the given argument represents a directory
+// and that directory's name matches the directory pattern.
+func (a Arguments) IsMatchingDir(fileInfo os.FileInfo) (bool, error) {
+	if !fileInfo.IsDir() {
+		return false, nil
+	}
+
+	if a.DirPattern == "" {
+		return true, nil
+	}
+
+	return path.Match(a.DirPattern, fileInfo.Name())
+}
+
+// Parses the command line arguments
+func parseArguments() Arguments {
+	var arguments Arguments
+	flag.StringVar(&arguments.Dir, "dir", ".", "The root directory containing other directories")
+	flag.StringVar(&arguments.DirPattern, "dir-pattern", "", "An optional wildcard pattern to select only some directories")
+	flag.BoolVar(&arguments.DryRun, "dry-run", false, "Don't actually run any command")
 
 	flag.Parse()
 
 	remainingArguments := flag.Args()
-
-	fmt.Println(remainingArguments)
-
 	if len(remainingArguments) <= 0 {
 		fmt.Println("Please specify the command that should be run")
 		os.Exit(1)
 	}
 
-	cmdName := remainingArguments[0]
-	args := remainingArguments[1:]
+	arguments.CommandName = remainingArguments[0]
+	arguments.CommandArguments = remainingArguments[1:]
+	return arguments
+}
 
-	fmt.Println(cmdName)
-	fmt.Println(args)
+func runCommand(arguments Arguments, dirName string) error {
+	fullDir := filepath.Join(arguments.Dir, dirName)
+	if arguments.DryRun {
+		fmt.Printf("Would have run command in %s", fullDir)
+		fmt.Println()
+		return nil
+	}
 
-	files, err := ioutil.ReadDir(dir)
+	fmt.Printf("Running command in %s", fullDir)
+	fmt.Println()
+
+	var out bytes.Buffer
+	cmd := arguments.CreateCommand()
+	cmd.Stdout = &out
+	cmd.Dir = fullDir
+	err := cmd.Run()
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf(out.String())
+	fmt.Println()
+	return nil
+}
+
+func main() {
+	arguments := parseArguments()
+
+	files, err := ioutil.ReadDir(arguments.Dir)
 	if err != nil {
 		log.Fatal(err)
 		os.Exit(1)
 	}
 
 	for _, file := range files {
-		if file.IsDir() {
-			var out bytes.Buffer
-			cmd := exec.Command(cmdName, args...)
-			cmd.Stdout = &out
-			cmd.Dir = filepath.Join(dir, file.Name())
-			err := cmd.Run()
+		isMatchingDir, err := arguments.IsMatchingDir(file)
+		if err != nil {
+			log.Fatal(err)
+		} else if isMatchingDir {
+			err := runCommand(arguments, file.Name())
 			if err != nil {
 				fmt.Println(err)
 			}
-
-			fmt.Printf("result: %q", out.String())
-			fmt.Println()
 		}
 	}
 }
