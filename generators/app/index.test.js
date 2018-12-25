f013412ecadb06f215619e89ba15980ca9a57a1c
Nikolaos Georgiou <Nikolaos.Georgiou@gmail.com>
2018-12-25 11:15:44 +0100

    adding tests

---
+++ generators/app/index.test.js
@@ -0,0 +1,48 @@
+const assert = require('yeoman-assert');
+const helpers = require('yeoman-test');
+
+describe('index', () => {
+  describe('unscoped', () => {
+    before(() => helpers.run(__dirname)
+      .withPrompts({
+        name: 'mylib',
+        description: 'my lib is awesome',
+      }));
+
+    it('should create the expected files', () => {
+      assert.file([
+        '.editorconfig',
+        '.eslintrc.js',
+        '.gitattributes',
+        '.gitignore',
+        '.npmignore',
+        '.travis.yml',
+        'package.json',
+        'README.md',
+        'src/index.js',
+        'src/index.test.js',
+      ]);
+    });
+
+    it('should have the expected package name', () => {
+      assert.jsonFileContent('package.json', {
+        name: 'mylib',
+      });
+    });
+  });
+
+  describe('scoped', () => {
+    before(() => helpers.run(__dirname)
+      .withPrompts({
+        name: 'mylib',
+        description: 'my lib is awesome',
+        scope: '@ngeor',
+      }));
+
+    it('should have the expected package name', () => {
+      assert.jsonFileContent('package.json', {
+        name: '@ngeor/mylib2',
+      });
+    });
+  });
+});
