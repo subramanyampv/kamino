0b27ca8df3d10866890b39c7409d07c19d075453
Nikolaos Georgiou <Nikolaos.Georgiou@gmail.com>
2019-01-13 09:25:48 +0100

    Bitbucket Pipelines environment variables

--- version_ci_bot/main.py
+++ version_ci_bot/main.py
@@ -1,3 +1,32 @@
+import os
+
+
+def is_ci():
+  return os.environ['CI']
+
+
+def ensure_tag_does_not_exist():
+  '''
+  Ensures that the version specified in the project file does not already
+  exist as a git tag.
+
+  Supported project files are pom.xml, package.json, setup.py.
+
+  Additionally, it ensures that the project version does not leave gaps
+  in the SemVer sequence.
+  '''
+  if not is_ci():
+    raise ValueError('Should only run in CI environments')
+
+
+
+def publish_tag():
+  '''
+  Creates a new tag based on the version specified in the project file.
+  '''
+  pass
+
+
 def main():
   print('version ci bot is under construction')
 
