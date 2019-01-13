6c5307d0c3fb33424c401497ddd91e34f47a85f1
Nikolaos Georgiou <Nikolaos.Georgiou@gmail.com>
2019-01-13 12:41:55 +0100

    Reading version from pom.xml file

--- version_ci_bot/test_bitbucket_pipelines.py
+++ version_ci_bot/test_bitbucket_pipelines.py
@@ -3,7 +3,8 @@ import os
 from unittest.mock import patch
 from version_ci_bot.bitbucket_pipelines import *
 
-@patch.dict('os.environ', { 'BITBUCKET_COMMIT': 'sha1' })
+
+@patch.dict('os.environ', {'BITBUCKET_COMMIT': 'sha1'})
 class BitbucketPipelinesTestCase(unittest.TestCase):
   def test_commit(self):
     result = commit()
