429ba67176cf3804881fe0a2371b6a60cae07249
Nikolaos Georgiou <Nikolaos.Georgiou@gmail.com>
2019-01-13 21:03:39 +0100

    0.0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
 
 setuptools.setup(
   name='version_ci_bot',
-  version='0.0.2',
+  version='0.0.3',
   author='Nikolaos Georgiou',
   author_email='Nikolaos.Georgiou@gmail.com',
   license='MIT',
