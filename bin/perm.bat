b8addcc19c78a3b09846d88e9c0b73e0f8c4cfb8
Nikolaos Georgiou <nikolaos.georgiou@gmail.com>
2020-03-08 13:44:21 +0100

    uncomment attrib commands

--- bin/perm.bat
+++ bin/perm.bat
@@ -1,26 +1,30 @@
 @ECHO ON
 
 REM turn archive bit off everywhere
-REM attrib -A /S /D .
+attrib -A /S /D .
 
 REM reset indexing bit
-REM attrib -I /S /D .
+attrib -I /S /D .
 
 REM remove indexing from these
-REM attrib +I /S /D Archive
-REM attrib +I /S /D .npm
-REM attrib +I /S /D .m2
-REM attrib +I /S /D Projects
-REM attrib +I /S /D AppData
-REM attrib +I /S /D vbox
+attrib +I /S /D Archive
+attrib +I /S /D .npm
+attrib +I /S /D .m2
+attrib +I /S /D Projects
+attrib +I /S /D AppData
+attrib +I /S /D vbox
 
 REM make Archive read-only
-REM attrib +R /S /D Archive
+attrib +R /S /D Archive
 
 REM disable compression everywhere
-compact /I /Q /U /A /S:C:\
+REM compact /I /Q /U /A /S:C:\
 
 REM enable compression in these folders
 compact /Q /C /A /S:Archive
 compact /Q /C /A /S:AppData
 compact /Q /C /A /S:Projects
+
+REM external drives disable compression
+compact /I /Q /U /A /S:E:\
+compact /I /Q /U /A /S:F:\
