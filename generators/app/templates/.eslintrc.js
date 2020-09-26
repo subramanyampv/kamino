7b94accb9a30959ba5c4c1e459be38446b391f9f
Nikolaos Georgiou <nikolaos.georgiou@gmail.com>
2020-09-26 21:59:51 +0200

    updated eslintrc of generated app

--- generators/app/templates/.eslintrc.js
+++ generators/app/templates/.eslintrc.js
@@ -1,11 +1,11 @@
 module.exports = {
-  "extends": "airbnb-base",
-  "env": {
-    "<%= testFramework %>": true
+  extends: "airbnb-base",
+  env: {
+    <%= testFramework %>: true
   },
-  "rules": {
-    "linebreak-style": 0,
-    "no-unused-expressions": 0,
-    "comma-dangle": ["error", "never"]
+  rules: {
+    'linebreak-style': 0,
+    'no-unused-expressions': 0,
+    'comma-dangle': ['error', 'never']
   }
 };
