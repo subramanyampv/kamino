8ce37a58ed0cffcb83a8b1b53863478434369b94
Nikolaos Georgiou <nikolaos.georgiou@gmail.com>
2020-10-18 13:18:51 +0200

    Not using hyper.js anymore

--- dotfiles/install.cmd
+++ dotfiles/install.cmd
@@ -2,16 +2,12 @@
 IF /I "%1"=="up" (
 	mklink "%USERPROFILE%\.gitconfig_include" "%CD%\gitconfig_include"
 	mklink "%USERPROFILE%\.bash_aliases" "%CD%\bash_aliases"
-	mklink "%USERPROFILE%\.clang-format" "%CD%\clang-format"
-	mklink "%APPDATA%\Hyper\.hyper.js" "%CD%\hyper.js"
 	GOTO:EOF
 )
 
 IF /I "%1"=="down" (
 	DEL "%USERPROFILE%\.gitconfig_include"
 	DEL "%USERPROFILE%\.bash_aliases"
-	DEL "%USERPROFILE%\.clang-format"
-	DEL "%APPDATA%\Hyper\.hyper.js"
 	GOTO:EOF
 )
 
