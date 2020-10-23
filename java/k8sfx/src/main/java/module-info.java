d6cc329129207e48e44979220350b59cfd892a3b
Nikolaos Georgiou <nikolaos.georgiou@gmail.com>
2020-10-23 16:42:56 +0200

    Removed java fx project

--- java/k8sfx/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module com.github.ngeor {
-    requires javafx.controls;
-    requires com.fasterxml.jackson.dataformat.yaml;
-    requires com.fasterxml.jackson.databind;
-    exports com.github.ngeor.k8sfx;
-    exports com.github.ngeor.kubeconfig;
-}
